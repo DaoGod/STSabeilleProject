d1bfbfba5c073aee0ec9c4457ecd7af3f0c3281d
Ammos <Ammos@LAPTOP-AMMOS>
2020-11-10 15:53:43 +0100

    Commit Maxence

---
+++ spring-abeilles/src/test/java/com/ynov/abeilles/SpringAbeillesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.abeilles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAbeillesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-recolte_production-service/src/main/java/com/ynov/abeilles/SpringRecolteProductionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.abeilles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecolteProductionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecolteProductionServiceApplication.class, args);
+	}
+
+}
