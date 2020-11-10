2f7db0580bb1fc2739c68c2e1e352efd7b3759dc
DaoGod <epistre31570@gmail.com>
2020-11-10 15:14:41 +0100

    first exemple commit

---
+++ spring-abeille/src/test/java/ynov/com/enzo_package/SpringAbeilleApplicationTests.java
@@ -0,0 +1,13 @@
+package ynov.com.enzo_package;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAbeilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
