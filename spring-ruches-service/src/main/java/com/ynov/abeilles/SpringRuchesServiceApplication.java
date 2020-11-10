7fe3336f8a6a78106ddad604acef4da62be4aeda
Ammos <Ammos@LAPTOP-AMMOS>
2020-11-10 17:56:22 +0100

    Clean Commit

---
+++ spring-ruches-service/src/main/java/com/ynov/abeilles/SpringRuchesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.abeilles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRuchesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRuchesServiceApplication.class, args);
+	}
+
+}
